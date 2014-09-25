eea6ae0f5321141f00633d8ba0dae257a1bb307a
31448674 <31448674@087819-P33S105.macklabs.br>
2014-09-25 11:36:19 -0300

    index/css geral/foto loign

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet " type="text/css" href=""/>
+    </head>
+    
+    <body>
+        <div id="main">
+            <nav id="mainNav">
+                <ul>
+                    <li><a>HOME</a></li>
+                    <li><a>PERFIL</a></li>
+                    <li><a>MATÉRIAS</a></li>
+                    <li><a>CADASTRO</a></li>
+                </ul>
+            </nav>
+        </div>
+        
+        <header>
+            
+        </header>
+        
+        
+              
+    </body>
+</html>
+<?php
+ 
+
+?>
