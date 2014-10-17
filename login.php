1edc8d718b94f9b84102dd9a004a41f4cb059c51
31448471 <31448471@087819-P33S105.macklabs.br>
2014-10-17 08:18:27 -0300

    login/login css

--- login.php
+++ login.php
@@ -3,12 +3,13 @@
     <head>
         <title></title>
         <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="login.css"/>
     </head>
     
     <body>
         <div>
             <form>
-                
+                <p>Nome: <input type="text" name="nome" size="20"></p>
             </form>
         </div> 
         
