6ad73a971d166cc68e17f672afdc0c0aa36cd264
Rodrigo Friedrich <Rodrigo Friedrich@192.168.0.12>
2014-11-06 15:47:11 -0200

    login

--- login.php
+++ login.php
@@ -42,6 +42,7 @@
 				background: -moz-radial-gradient(50% 46% 90deg,circle closest-corner, #242424, #090909);
 				background: -webkit-gradient(radial, 50% 50%, 0, 50% 50%, 150, from(#242424), to(#090909));
 			}
+                        
 			form label{
 				display: block;
 				padding: 10px 10px 5px 15px;
@@ -73,16 +74,17 @@
 		<div class="rain">
 			<div class="border start">
                             
-				<form method="POST" action="cadastra.php"> 
+				<form method="POST"> 
                                     <!-- Deve redirecionar para a página de validação -->
 					<label for="email">Email</label>
 					<input name="email" type="text" placeholder="Email"/>
 					<label for="pass">Password</label>
 					<input name="pass" type="password" placeholder="Password"/>
                                         <input type="submit" value="LOG IN">
-                                        <input type="submit" value="CADASTRAR" action="etapa1.php"/>
+                                        <input type="button" value="CADASTRAR" action="etapa1.php"/>
+                                         
                                 </form>      
-                                        
+                               
                                        
                                 
 			</div>
