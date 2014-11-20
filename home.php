8803b18d72d6c6cb95b90769fd2fec8b73013510
Hugo Chimello <hugoluizchimello@gmail.com>
2014-11-20 17:02:46 -0200

    Timeline funcionando porem sem css propio
    OBS.: posts devem ser feitos dentro do banco

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
         <title>FEEG - Home</title> 
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/entrar.css">
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
     </head>
 	<body>
@@ -12,15 +13,58 @@
                 //session_start();
                 if(isset($_SESSION['FEEGusuario'])){
                     $usuario = $_SESSION['FEEGusuario'];
-                    echo $usuario;
-                    session_destroy();
                 }
-                else{
-                    echo "erro porra";
-                }
+                
+                $usuarioIMG="<img src='imagens/person.png' alt='profile pic'/>";
             ?>
 		<div id="main">
-                    
+                    <header >
+                        <nav class="barra" >
+                            <ul>
+                                <img src="imagens/FeegLogo.png" alt="FEEG logo"/>
+                                <div>
+                                    <li><?php echo "$usuario"?></li>
+                                    <li><?php echo "$usuarioIMG"?></li>
+                                </div>
+                            </ul>                            
+                        </nav>
+                    </header>
+                    <aside>
+                        <div class="profile">
+                            <header>
+                                <?php echo "$usuarioIMG"?>
+                                <b><a><?php echo "$usuario"?> </a> </b>
+                            </header>
+                        </div>
+                    </aside>
+                    <section>
+                        <?php
+                        //1. SQL da tabela timline que todos os posts
+                         $sql = "SELECT * FROM timeline";
+                         $resultado = mysqli_query($conexao, $sql);
+
+                         //2. Mostrar Resultados por amigo
+                         if($resultado){
+                            while($timeline=mysqli_fetch_array($resultado)){
+                                $post=$timeline['conteudo'];
+                                $username=$timeline['username'];
+                                echo <<<POST
+                                <article class='post'>
+                                    <header>
+                                        <p>$usuario</p>
+                                        <p>$usuarioIMG</p>
+                                    </header>
+                                    <p>$post</p>
+                                </article>
+POST;
+                            }
+                         }
+
+                         //3. Fechar conexão
+                         mysqli_close($conexao);
+
+                        ?>  
+                    </section>
 		</div>
 	</body>
 </html>
