d72b67bf45db3ca8f7a45665eaa482c8fd3b1c5e
Hugo Chimello <hugoluizchimello@gmail.com>
2014-11-20 02:16:15 -0200

    Mudancas em relacao ao banco de dados e a reestruturacao, cadastro e sessao funcionando!

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>FEEG - Home</title>
+        <title>FEEG - Home</title> 
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
